acafec601eb70cff0d05159ab1264875b1bcaa0f
Sten Robbin Edgren <mek@pels.in>
2019-06-03 01:19:46 +0200

    Linenumber directives from markdown files
    With this patch you get line directives (common and golang-specific) in
    your output. Useful when debugging, by rightclicking on acme or with gF
    in vim. Typical output with `go run' or `go build':
    
      # command-line-arguments
      WhitespacePreservation.md:38: invalid character U+0023 '#'
      WhitespacePreservation.md:38: undefined: TEST
      WhitespacePreservation.md:39: missing return at end of function
      LineNumbers.md:147: undefined: parseHeader
    
    This is could be seen as an improvment, instead of finding a bug in your
    go code and then trying to find the right place in your md-files. It is
    however very verbose (in your output), it is however "easy" to remove
    the directives when you want to publish your go-code:
    
      sed -i '/^\/\/line/d' main.go

--- main.go
+++ main.go
@@ -1,38 +1,64 @@
+//line README.md:65
 package main
 
 import (
-	"bufio"
+//line README.md:149
 	"fmt"
-	"io"
 	"os"
-	"path/filepath"
+	"io"
+//line README.md:212
+	"bufio"
+//line README.md:385
 	"regexp"
+//line README.md:510
 	"strings"
+//line SubdirectoryFiles.md:35
+	"path/filepath"
+//line README.md:69
 )
 
+//line LineNumbers.md:25
 type File string
-type CodeBlock string
+type CodeBlock []CodeLine
 type BlockName string
+type language string
+//line LineNumbers.md:36
+type CodeLine struct {
+	text   string
+	file   File
+	lang   language
+	number int
+}
+//line LineNumbers.md:30
 
 var blocks map[BlockName]CodeBlock
 var files map[File]CodeBlock
+//line README.md:402
 var namedBlockRe *regexp.Regexp
+//line README.md:432
 var fileBlockRe *regexp.Regexp
+//line README.md:516
 var replaceRe *regexp.Regexp
+//line README.md:72
 
+//line LineNumbers.md:118
 // Updates the blocks and files map for the markdown read from r.
-func ProcessFile(r io.Reader) error {
+func ProcessFile(r io.Reader, inputfilename string) error {
+//line LineNumbers.md:82
 	scanner := bufio.NewReader(r)
 	var err error
-	var line string
+
+	var line CodeLine
+	line.file = File(inputfilename)
 
 	var inBlock, appending bool
 	var bname BlockName
 	var fname File
 	var block CodeBlock
-
+//line LineNumbers.md:99
 	for {
-		line, err = scanner.ReadString('\n')
+		line.text, err = scanner.ReadString('\n')
+		line.number++
 		switch err {
 		case io.EOF:
 			return nil
@@ -41,12 +67,15 @@ func ProcessFile(r io.Reader) error {
 		default:
 			return err
 		}
+//line LineNumbers.md:145
 		if inBlock {
-			if line == "```\n" {
+			if line.text == "```\n" {
+//line LineNumbers.md:56
+				inBlock = false
 				// Update the files map if it's a file.
 				if fname != "" {
 					if appending {
-						files[fname] += block
+						files[fname] = append(files[fname], block...)
 					} else {
 						files[fname] = block
 					}
@@ -55,108 +84,136 @@ func ProcessFile(r io.Reader) error {
 				// Update the named block map if it's a named block.
 				if bname != "" {
 					if appending {
-						blocks[bname] += block
+						blocks[bname] = append(blocks[bname], block...)
 					} else {
 						blocks[bname] = block
 					}
 				}
-
-				inBlock = false
-				appending = false
-				bname = ""
-				fname = ""
-				block = ""
-				continue
-			} else {
-				block += CodeBlock(line)
-			}
-		} else {
-			if line == "" {
+//line LineNumbers.md:148
 				continue
 			}
-
-			switch line[0] {
-			case '`':
-				if len(line) >= 3 && line[0:3] == "```" {
-					inBlock = true
-					fname, bname, appending = parseHeader(line)
-					// We're outside of a block, so just blindly reset it.
-					block = ""
-				}
-			default:
-				inBlock = false
-				appending = false
-				bname = ""
-				fname = ""
-				block = ""
-			}
+//line LineNumbers.md:48
+			block = append(block, line)
+//line LineNumbers.md:151
+			continue
 		}
+//line LineNumbers.md:170
+		if len(line.text) >= 3 && (line.text[0:3] == "```") {
+			inBlock = true
+			// We were outside of a block, so just blindly reset it.
+			block = make(CodeBlock, 0)
+//line LineNumbers.md:181
+			fname, bname, appending, line.lang = parseHeader(line.text)
+//line LineNumbers.md:175
+		}
+//line LineNumbers.md:111
 	}
+//line LineNumbers.md:121
 }
-func parseHeader(line string) (File, BlockName, bool) {
+//line LineNumbers.md:190
+func parseHeader(line string) (File, BlockName, bool, language) {
 	line = strings.TrimSpace(line)
-	matches := namedBlockRe.FindStringSubmatch(line)
-	if matches != nil {
-		return "", BlockName(matches[2]), (matches[3] == "+=")
+//line LineNumbers.md:197
+	var matches []string
+	if matches = namedBlockRe.FindStringSubmatch(line); matches != nil {
+		return "", BlockName(matches[2]), (matches[3] == "+="), language(matches[1])
 	}
-	matches = fileBlockRe.FindStringSubmatch(line)
-	if matches != nil {
-		return File(matches[2]), "", (matches[3] == "+=")
+	if matches = fileBlockRe.FindStringSubmatch(line); matches != nil {
+		return File(matches[2]), "", (matches[3] == "+="), language(matches[1])
 	}
-	return "", "", false
+	return "", "", false, ""
+//line LineNumbers.md:193
 }
-
+//line WhitespacePreservation.md:34
 // Replace expands all macros in a CodeBlock and returns a CodeBlock with no
 // references to macros.
 func (c CodeBlock) Replace(prefix string) (ret CodeBlock) {
-	scanner := bufio.NewReader(strings.NewReader(string(c)))
-
-	for {
-		line, err := scanner.ReadString('\n')
-		// ReadString will eventually return io.EOF and this will return.
-		if err != nil {
-			return
-		}
+//line LineNumbers.md:251
+	var line string
+	for _, v := range c {
+		line = v.text
+//line LineNumbers.md:234
 		matches := replaceRe.FindStringSubmatch(line)
 		if matches == nil {
-			ret += CodeBlock(prefix) + CodeBlock(line)
+			if v.text != "\n" {
+				v.text = prefix + v.text
+			}
+			ret = append(ret, v)
 			continue
 		}
+//line LineNumbers.md:220
 		bname := BlockName(matches[2])
 		if val, ok := blocks[bname]; ok {
-			ret += val.Replace(prefix + matches[1])
+			ret = append(ret, val.Replace(prefix+matches[1])...)
 		} else {
 			fmt.Fprintf(os.Stderr, "Warning: Block named %s referenced but not defined.\n", bname)
-			ret += CodeBlock(line)
+			ret = append(ret, v)
 		}
+//line LineNumbers.md:255
+	}
+	return
+//line WhitespacePreservation.md:38
+}
+//line LineNumbers.md:277
+
+// Finalize extract the textual lines from CodeBlocks and (if needed) prepend a
+// notice about "unexpected" filename or line changes, which is extracted from
+// the contained CodeLines. The result is a string with newlines ready to be
+// pasted into a file.
+func (c CodeBlock) Finalize() (ret string) {
+	var file File
+	var formatstring string
+	var linenumber int
+	for _, l := range c {
+		if linenumber+1 != l.number || file != l.file {
+			switch l.lang {
+			case "go", "golang":
+				formatstring = "//line %[2]v:%[1]v\n"
+			default:
+				formatstring = "#line %v \"%v\"\n"
+			}
+			ret += fmt.Sprintf(formatstring, l.number, l.file)
+		}
+		ret += l.text
+		linenumber = l.number
+		file = l.file
 	}
 	return
 }
+//line README.md:74
 
 func main() {
+//line README.md:157
 	// Initialize the maps
 	blocks = make(map[BlockName]CodeBlock)
 	files = make(map[File]CodeBlock)
-	namedBlockRe = regexp.MustCompile("^([`]+\\s?)[\\w]+[\\s]+\"(.+)\"[\\s]*([+][=])?$")
-	fileBlockRe = regexp.MustCompile("^([`]+\\s?)[\\w]+[\\s]+([\\w\\.\\-\\/]+)[\\s]*([+][=])?$")
+//line LineNumbers.md:208
+	namedBlockRe = regexp.MustCompile("^`{3,}\\s?(\\w*)\\s*\"(.+)\"\\s*([+][=])?$")
+//line LineNumbers.md:212
+	fileBlockRe = regexp.MustCompile("^`{3,}\\s?(\\w+)\\s+([\\w\\.\\-\\/]+)\\s*([+][=])?$")
+//line WhitespacePreservation.md:11
 	replaceRe = regexp.MustCompile(`^([\s]*)<<<(.+)>>>[\s]*$`)
+//line README.md:136
 
 	// os.Args[0] is the command name, "lmt". We don't want to process it.
 	for _, file := range os.Args[1:] {
+//line LineNumbers.md:127
 		f, err := os.Open(file)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "error: ", err)
 			continue
 		}
 
-		if err := ProcessFile(f); err != nil {
+		if err := ProcessFile(f, file); err != nil {
 			fmt.Fprintln(os.Stderr, "error: ", err)
 		}
 		// Don't defer since we're in a loop, we don't want to wait until the function
 		// exits.
 		f.Close()
+//line README.md:140
 
 	}
+//line LineNumbers.md:308
 	for filename, codeblock := range files {
 		if dir := filepath.Dir(string(filename)); dir != "." {
 			if err := os.MkdirAll(dir, 0775); err != nil {
@@ -169,9 +226,9 @@ func main() {
 			fmt.Fprintf(os.Stderr, "%v\n", err)
 			continue
 		}
-		fmt.Fprintf(f, "%s", codeblock.Replace(""))
+		fmt.Fprintf(f, "%s", codeblock.Replace("").Finalize())
 		// We don't defer this so that it'll get closed before the loop finishes.
 		f.Close()
-
 	}
+//line README.md:77
 }
